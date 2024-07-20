0c85ecba3d8d910a7d2fb84dc8149c566edee6d7
Geraldine <geraldine.garces@soymas.cl>
2024-07-20 01:02:31 -0400

    changes

--- prompt.js
+++ prompt.js
@@ -1,9 +1,10 @@
 alert("Bienvenido al número secreto")
 let userChoice = prompt("Elige un número entre 1 y 10")
 alert ("Tu elección fue:" + userChoice)
-let secretNumber = Math.random()*10
+let secretNumber =Math.floor(Math.random()*10)
 
 if(userChoice == secretNumber){
     alert("Ganaste!")
-    else ("¡Perdiste! el número secreto era:" + secretNumber )
+   }   else {
+       alert("¡Perdiste! el número secreto era:" + secretNumber)
 }
